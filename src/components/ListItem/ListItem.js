44e72ab0d6feb476eaea1b54cd2d9ef234b52bd7
daoyi7 <kawhichina@gmail.com>
2017-11-16 14:09:23 +0800

    update Component -> detail.js

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,36 +1,39 @@
 import React, {Component} from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
 import moment from 'moment'
+import Detail from '../Detail/Detail'
 
 export default class ListItem extends Component {
-
-  componentDidMount() {
-    console.log(moment.unix(1510666557).format())
-  }
 
   render() {
     const list = this.props.list
 
     return (
-      <div className="listItems">
-        <div className="member">
-          <img className="avatar" src={list.member.avatar_normal} alt="" />
-        </div>
-        <div className="list">
-          <div className="node">
-            <span className="nodename">{list.node.title}</span>
-            <span className="username">{list.member.username}</span>
+      <BrowserRouter basename="/">
+        <div className="listItems">
+          <div className="member">
+            <img className="avatar" src={list.member.avatar_normal} alt="" />
           </div>
-          <h3 className="title">{list.title}</h3>
-          <div className="timebox">
-            <span className="last_modified">
-              {
-                moment(moment.unix(list.last_modified).format()).fromNow()
-              }
-            </span>
+          <div className="list">
+            <div className="node">
+              <span className="nodename">{list.node.title}</span>
+              <span className="username">{list.member.username}</span>
+            </div>
+            <h3 className="title">
+              <Link to={`/detail/${list.id}`}>{list.title}</Link>
+            </h3>
+            <div className="timebox">
+              <span className="last_modified">
+                {
+                  moment(moment.unix(list.last_modified).format()).fromNow()
+                }
+              </span>
+            </div>
           </div>
+          <span className="replies">{list.replies}</span>
+          <Route path="/detail/:id" component = {Detail}></Route>
         </div>
-        <span className="replies">{list.replies}</span>
-      </div>
+      </BrowserRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './components/css/reset.css';
-import './components/css/app.css';
+import './components/static/css/reset.css';
+import './components/static/css/app.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
